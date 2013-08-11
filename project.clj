3c7ae67f7a45028c738b787e34cb340a9a5ff026
John Napier <jnape09@gmail.com>
2013-08-11 17:19:10 -0400

    Adding documentation; updating project.clj with correct URL and marg plugin

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.clojars.jnape/unpackr "0.1.0-SNAPSHOT"
+(defproject unpackr "0.1.0-SNAPSHOT"
   :description "DSL for making working with byte arrays palatable in Clojure"
-  :url "http://www.jnape.org/code/unpackr"
+  :url "http://www.jnape.com/code/unpackr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [commons-lang/commons-lang "2.6" :scope "test"]])
+                 [commons-lang/commons-lang "2.6" :scope "test"]]
+  :plugins [[lein-marginalia "0.7.1"]])
--- src/unpackr/core.clj
+++ src/unpackr/core.clj
@@ -29,17 +29,57 @@
   (let [pairs (partition 2 coll)]
     [(map first pairs) (map second pairs)]))
 
-(defn as-bytes [l]
+(defn as-bytes
+  "Creates a byte-array from a seq of numbers.
+
+   Note that all numbers must be between -128 and 127 inclusive, or else an ```IllegalArgumentException``` is thrown."
+  [l]
   (byte-array (map byte l)))
 
-(defn buffer-from [barr]
+(defn buffer-from
+  "Wraps a byte array with a ```java.nio.ByteBuffer```"
+  [barr]
   (ByteBuffer/wrap barr))
 
-(defn unpack [fmts barr]
+(defn unpack
+  "Unpacks values corresponding to the specified formats from the byte-array.
+
+  Valid formats are:
+
+  ```:byte```      - Extract the next byte as a signed byte
+
+  ```:ubyte```     - Extract the next byte as an unsigned byte
+
+  ```:short```     - Extract the next 2 bytes as a signed short
+
+  ```:ushort```    - Extract the next 2 bytes as an unsigned short
+
+  ```:int```       - Extract the next 4 bytes as a signed int
+
+  ```:uint```      - Extract the next 4 bytes as an unsigned int
+
+  ```:rest```      - Extract the remaining bytes as a byte array
+
+  ```n (number)``` - Extract the next ```n``` bytes as a byte array
+
+  Example usage:
+
+    (unpack [:short :uint 10] (as-bytes (range 100)))
+    ;-> [1 33752069 #<byte[] [B@3761cf73>]```"
+  [fmts barr]
   (let [buffer (buffer-from barr)]
     (map #((unpack-format %) buffer) fmts)))
 
-(defmacro unpack-let [bindings barr & body]
+(defmacro unpack-let
+  "Unpacks values and immediately binds them to specified vars in a let clause.
+
+  Example usage:
+
+    (unpack-let [x :short y :int] (as-bytes [0 1 0 0 0 10])
+      (+ x y))
+    ;-> 11
+  "
+  [bindings barr & body]
   (let [[ks vs] (unzip bindings)]
     `(let [~(vec ks) (unpack ~(vec vs) ~barr)]
        ~@body)))
